36efc8e55c762a981c6dc1f3b7ef2633a350016c
Esteban Hidalgo <hidalgo.esteban@2594@gmail.com>
2022-06-07 14:50:33 -0600

    feat: Added the preview of the markdown text on the other side (still not parsed)

--- src/components/UI/TextContent/MarkdownEditor.tsx
+++ src/components/UI/TextContent/MarkdownEditor.tsx
@@ -1,17 +1,27 @@
-import React, { ChangeEventHandler } from 'react';
+import React, { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import marked from 'markdown-it';
 import './MarkdownEditor.scss';
 
 const MarkdownEditor: React.FC<MarkdownEditorProps> = (props) => {
   const { onChange, className } = props;
+
+  const [markdownText, setMarkdownText] = useState('');
+
+  const markdownTextChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdownText(e.target.value);
+    console.log(markdownText);
+  };
 
   return (
     <div className='main__editor'>
       <textarea
         cols={30}
         rows={10}
-        onChange={onChange}
+        onChange={markdownTextChangeHandler}
         className={`main__editor-textarea ${className}`}
+        value={markdownText}
       ></textarea>
+      <div className='main__editor-preview'>{marked.parse(markdownText)}</div>
     </div>
   );
 };
