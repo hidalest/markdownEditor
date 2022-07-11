922460d56263b37a9ccb5f0a981e23b936edc67a
Esteban Hidalgo <estebanhidalgo@Estebans-MacBook-Air.local>
2022-07-10 22:53:47 -0600

    added the save and delete button

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import DataContextProvider from './context/DataContextProvider';
 
 function App() {
   const [activeSidebar, setActiveSidebar] = useState(false);
+  const [showPreview, setShowPreview] = useState(false);
   const toggleSidebarHandler = () => setActiveSidebar(!activeSidebar);
 
   const activeSidebarClass = activeSidebar ? 'sidebarActive' : '';
+  const showPreviewClass = showPreview ? 'show-preview' : 'hide-preview';
 
   return (
     <DataContextProvider>
@@ -16,8 +18,22 @@ function App() {
         <Navbar isActive={activeSidebar} onClick={toggleSidebarHandler} />
         <main className='main'>
           <div className='main__statusBar'>
-            <h3>MARKDOWN</h3>
-            <h3>PREVIEW</h3>
+            <div className='main__statusBar-actions main__statusBar-actions-markdown'>
+              <h3>MARKDOWN</h3>
+              <button>
+                <span className='material-symbols-outlined main__statusBar-actions-visibleIcon'>
+                  visibility
+                </span>
+              </button>
+            </div>
+            <div className='main__statusBar-actions main__statusBar-actions-preview'>
+              <h3>PREVIEW</h3>
+              <button>
+                <span className='material-symbols-outlined main__statusBar-actions-invisibleIcon'>
+                  visibility_off
+                </span>
+              </button>
+            </div>
           </div>
           <MarkdownEditor className={''} />
         </main>
--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -2,18 +2,36 @@ import React, { MouseEventHandler } from 'react';
 import './Navbar.scss';
 import Sidebar from '../Sidebar/Sidebar';
 import HamburgerButton from '../../UI/Hamburger/Hamburger';
+import Button from '../../UI/Button/Button';
 
 const Navbar: React.FC<NavbarProps> = ({ isActive, onClick }) => {
+  const onSaveClickHandler = () => {};
+
   return (
     <nav className='navbar'>
       <Sidebar isActive={isActive} />
-      <HamburgerButton
-        isActive={isActive}
-        className={'navbar__hamburger'}
-        onClick={onClick}
-      />
-      <h1 className='navbar__title'>Markdown</h1>
-      <span className='navbar__separator'></span>
+      <div className='navbar__actions'>
+        <HamburgerButton
+          isActive={isActive}
+          className={'navbar__hamburger'}
+          onClick={onClick}
+        />
+        <h1 className='navbar__title'>Markdown</h1>
+        <span className='navbar__separator'></span>
+      </div>
+
+      <div className='navbar__actions'>
+        <button className='navbar__actions-btnDelete'>
+          <span className='material-symbols-outlined '>delete</span>
+        </button>
+        <Button
+          className='navbar__actions-btnSave '
+          buttonType='save'
+          onClick={onSaveClickHandler}
+        >
+          {'Save Changes'}
+        </Button>
+      </div>
     </nav>
   );
 };
